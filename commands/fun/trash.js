f897eef19beb9d72a1d7be6f2588b0305dc4eb9d
Meesam <70945820+meesam4687@users.noreply.github.com>
2023-01-19 14:49:28 +0530

    Update trash.js

--- commands/fun/trash.js
+++ commands/fun/trash.js
@@ -15,7 +15,7 @@ module.exports = {
         let tgt = interaction.options.getUser('user')
         var av = tgt.displayAvatarURL({ dynamic: false, format: 'png' })
         let img = await new imgGen.Trash().getImage(av);
-        let attach = new Discord.MessageAttachment(img, "inverted.png");
+        let attach = new Discord.AttachmentBuilder(img, "inverted.png");
         interaction.reply({ files: [attach] })
     },
 };
