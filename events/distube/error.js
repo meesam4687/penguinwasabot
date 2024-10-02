e46d1c8ef2bee4bbd679a9283d6d54ec73699077
Meesam <70945820+meesam4687@users.noreply.github.com>
2024-10-02 10:47:23 +0000

    give a more clear message on error

--- events/distube/error.js
+++ events/distube/error.js
@@ -6,6 +6,10 @@ module.exports = {
 		  queue.textChannel.send('I can\'t Join that Voice Channel')
 		  return;
 	  }
+	  if(error.code === 'FFMPEG_EXITED'){
+		  queue.textChannel.send('YouTube is cracking down on bots that fetch songs, hence I wasnt able to fetch the audio and play the song.')
+		  return;
+	  }
 	  queue.textChannel.send(`An Error Occured While Executing That Command \n||${error}||`)
           console.log(error)
   },
