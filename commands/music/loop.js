a082991ab4a87de94dd310d0a73fe2f90344485b
Meesam <meesam4687@gmail.com>
2025-06-23 05:54:12 +0000

    feat: re-added loop command

---
+++ commands/music/loop.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    data: new Discord.SlashCommandBuilder()
+        .setName('loop')
+        .setDescription('Loop the song currently playing')
+        .addStringOption(option =>
+            option.setName('option')
+                .setDescription('On or Off')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'On', value: 'track' },
+                    { name: 'Off', value: 'off' },
+                )),
+    async execute(interaction) {
+        const player = interaction.client.moonlinkManager.players.get(interaction.guild.id);
+        player.setLoop(interaction.options.getString('option'));
+        const loopStatus = interaction.options.getString('option') === 'track' ? 'enabled' : 'disabled';
+        interaction.reply({content: `Loop ${loopStatus}`}
+        );
+    },
+};
