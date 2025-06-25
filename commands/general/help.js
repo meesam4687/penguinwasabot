401756b5a753085633a9ad45f33bd389b3d3ffeb
meesam4687 <meesam4687@gmail.com>
2025-06-25 21:00:25 +0530

    feat: added the help command back

--- commands/general/help.js
+++ commands/general/help.js
@@ -1,4 +1,36 @@
 const Discord = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+
+let musicCommands = [];
+try {
+  musicCommands = fs
+    .readdirSync(path.join(__dirname, "../music/"))
+    .filter((file) => file.endsWith(".js"))
+    .map((file) => `\`/${path.parse(file).name}\``);
+} catch (err) {
+  console.error("Error reading music commands:", err);
+}
+
+let generalCommands = [];
+try {
+  generalCommands = fs
+    .readdirSync(path.join(__dirname, "../general/"))
+    .filter((file) => file.endsWith(".js"))
+    .map((file) => `\`/${path.parse(file).name}\``);
+} catch (err) {
+  console.error("Error reading general commands:", err);
+}
+
+let funCommands = [];
+try {
+  funCommands = fs
+    .readdirSync(path.join(__dirname, "../fun/"))
+    .filter((file) => file.endsWith(".js"))
+    .map((file) => `\`/${path.parse(file).name}\``);
+} catch (err) {
+  console.error("Error reading fun commands:", err);
+}
 
 module.exports = {
   data: new Discord.SlashCommandBuilder()
@@ -10,7 +42,15 @@ module.exports = {
       .setColor("#57a3bd")
       .setTitle("Help")
       .setURL("https://www.youtube.com/watch?v=0iaNqJN5MTM")
-      .setDescription(`Bot is being re-written`);
+      .setDescription(`List of commands:`)
+      .addFields(
+        { name: "━━ 🎶 ・ Music commands ━━", value: musicCommands.join(", ") },
+        {
+          name: "━━ 🤖 ・ General commands ━━",
+          value: generalCommands.join(", "),
+        },
+        { name: "━━ 🏓 ・ Fun commands ━━", value: funCommands.join(", ") }
+      );
     interaction.reply({ embeds: [infoEmbed] }).catch(console.error);
   },
 };
